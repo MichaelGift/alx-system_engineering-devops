3c01b0dfc244b82a88e3d87bc22ab0f1d488cfca
MichaelGift <simiyugift@outlook.com>
2024-02-20 03:54:52 +0300

    Fix: PEP8 style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-'''This module contains a script to gather data from an API
-'''
+"""This module contains a script to gather data from an API
+"""
 import re
 import requests
 import sys
 
 
 API_URL = 'https://jsonplaceholder.typicode.com'
-'''The API's URL.'''
+'''The APIs URL.'''
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            user_id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, user_id)).json()
             todos_res = requests.get('{}/todos'.format(API_URL)).json()
             user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos = list(filter(lambda x: x.get('userId') == user_id, todos_res))
             todos_done = list(filter(lambda x: x.get('completed'), todos))
             print(
                 'Employee {} is done with tasks({}/{}):'.format(
