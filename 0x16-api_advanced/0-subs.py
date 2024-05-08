9251bc233e56e39a78ffcb60e4ed03a068b2b1bc
Michael Gift <simiyugift@outlook.com>
2024-05-08 03:42:12 +0300

    Feat: Get subreddit subscriber number
    Network call to fetch number of subscribers in a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module contains a function to query Reddit API for subreddit subscribers
+"""
+import requests
+
+BASE_URL = """https://www.reddit.com"""
+"""Reddit's base API url"""
+
+
+def number_of_subscribers(subreddit):
+    """
+    GETs the number of subscribers in a given subreddit.
+    :param subreddit: target subreddit name
+    """
+
+    header = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebkit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edge/97.0.1072.62'
+        ])
+    }
+
+    result = requests.get(
+        '{}/r/{}/about.json'.format(BASE_URL, subreddit),
+        headers=header,
+        allow_redirects=False
+    )
+
+    if result.status_code == 200:
+        return result.json()['data']['subscribers']
+    return 0
